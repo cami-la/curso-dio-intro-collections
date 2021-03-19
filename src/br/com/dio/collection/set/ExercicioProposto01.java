dac18e0a8fa6202bf7fa88a8c83aff2749a6a2f8
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2021-03-19 06:15:14 -0300

    Final Set

--- src/br/com/dio/collection/set/ExercicioProposto01.java
+++ src/br/com/dio/collection/set/ExercicioProposto01.java
@@ -1,4 +1,7 @@
 package br.com.dio.collection.set;
+
+import java.util.*;
+
 /*
 Crie uma conjunto contendo as cores do arco-íris e:
 a) Exiba todas as cores o arco-íris uma abaixo da outra;
@@ -12,6 +15,51 @@ h) Confira se o conjunto está vazio;
  */
 public class ExercicioProposto01 {
     public static void main(String[] args) {
+        System.out.println("Crie uma conjunto contendo as cores do arco-íris: ");
+        Set<String> coresArcoIris = new HashSet<>();
+        coresArcoIris.add("violeta");
+        coresArcoIris.add("anil");
+        coresArcoIris.add("azul");
+        coresArcoIris.add("verde");
+        coresArcoIris.add("amarelo");
+        coresArcoIris.add("laranja");
+        coresArcoIris.add("vermelho");
+        System.out.println(coresArcoIris);
 
+        System.out.println("Exiba todas as cores o arco-íris uma abaixo da outra: ");
+        for (String cor : coresArcoIris) {
+            System.out.println(cor);
+        }
+
+        System.out.println("A quantidade de cores que o arco-íris tem: " + coresArcoIris.size());
+
+        System.out.println("Exiba as cores em ordem alfabética: ");
+        Set<String> coresArcoIris2 = new TreeSet<>(coresArcoIris);
+        System.out.println(coresArcoIris2);
+
+        System.out.println("Exiba as cores na ordem inversa da que foi informada ");
+        Set<String> coresArcoIris3 = new LinkedHashSet<>(
+                Arrays.asList("violeta", "anil", "azul", "verde", "amarelo", "laranja", "vermelho"));
+        System.out.println(coresArcoIris3);
+        List<String> coresArcoIrisList = new ArrayList<>(coresArcoIris3);
+        Collections.reverse(coresArcoIrisList);
+        System.out.println(coresArcoIrisList);
+
+        System.out.println("Exiba todas as cores que começam com a letra ”v”: ");
+        for (String cor: coresArcoIris) {
+            if(cor.startsWith("v")) System.out.println(cor);
+        }
+
+        System.out.println("Remova todas as cores que não começam com a letra “v”: ");
+        Iterator<String> iterator2 = coresArcoIris.iterator();
+        while (iterator2.hasNext()) {
+            if (iterator2.next().startsWith("v")) iterator2.remove();
+        }
+        System.out.println(coresArcoIris);
+
+        System.out.println("Limpe o conjunto: ");
+        coresArcoIris.clear();
+
+        System.out.println("Confira se o conjunto está vazio: " + coresArcoIris.isEmpty());
     }
 }
--- src/br/com/dio/collection/set/ExercicioProposto02.java
+++ src/br/com/dio/collection/set/ExercicioProposto02.java
@@ -1,17 +1,121 @@
 package br.com.dio.collection.set;
+
+import java.util.*;
+
 /*
 Crie uma classe LinguagemFavorita que possua os atributos nome, anoDeCriacao e IDE.
 Em seguida, crie um conjunto com 3 linguagens e faça um programa que ordene esse conjunto por:
 a) Ordem de inserção;
-b) Ordem natural;
-b) Nome;
-c) Gênero;
-d) Tempo de Episódio;
-c) Nome, gênero e tempoEpisodio;
+b) Ordem natural(nome);
+c) IDE;
+d) Ano de criação e nome;
+e) Nome, ano de criação e IDE;
 Ao final, exiba as linguagens no console, um abaixo da outra.
 */
 public class ExercicioProposto02 {
     public static void main(String[] args) {
+        Set<LinguagemFavorita> minhasLinguagensFavoritas = new HashSet<>();
+        minhasLinguagensFavoritas.add(new LinguagemFavorita("Python", 1991, "Pycharm"));
+        minhasLinguagensFavoritas.add(new LinguagemFavorita("JavaScript", 1995, "IntelliJ"));
+        minhasLinguagensFavoritas.add(new LinguagemFavorita("Java", 1991, "Visual Studio Code"));
 
+        System.out.println("--------\tOrdem de Inserção\t--------");
+        Set<LinguagemFavorita> minhasLinguagensFavoritas1 = new LinkedHashSet<>(
+                Arrays.asList(
+                        new LinguagemFavorita("Python", 1991, "Pycharm"),
+                        new LinguagemFavorita("JavaScript", 1995, "IntelliJ"),
+                        new LinguagemFavorita("Java", 1991, "Visual Studio Code")
+                )
+        );
+        for (LinguagemFavorita linguagem : minhasLinguagensFavoritas1) System.out.println(linguagem);
+
+        System.out.println("--------\tOrdem Natural: Nome\t--------");
+        Set<LinguagemFavorita> minhasLinguagensFavoritas2 = new TreeSet<>(minhasLinguagensFavoritas);
+        for (LinguagemFavorita linguagem : minhasLinguagensFavoritas2) System.out.println(linguagem);
+
+        System.out.println("--------\tOrdem IDE\t--------");
+        Set<LinguagemFavorita> minhasLinguagensFavoritas3 = new TreeSet<LinguagemFavorita>(new ComparatorIde());
+        minhasLinguagensFavoritas3.addAll(minhasLinguagensFavoritas);
+        for (LinguagemFavorita linguagem : minhasLinguagensFavoritas3) System.out.println(linguagem);
+
+        System.out.println("--------\tOrdem Ano de Criacao e Nome\t--------");
+        Set<LinguagemFavorita> minhasLinguagensFavoritas4 = new TreeSet<LinguagemFavorita>(new ComparatorAnoDeCriacaoENome());
+        minhasLinguagensFavoritas4.addAll(minhasLinguagensFavoritas);
+        for (LinguagemFavorita linguagem : minhasLinguagensFavoritas4) System.out.println(linguagem);
+
+        System.out.println("--------\tOrdem Nome - Ano de Criacao - IDE\t--------");
+        Set<LinguagemFavorita> minhasLinguagensFavoritas5 = new TreeSet<LinguagemFavorita>(new ComparatorNomeAnoDeCriacaoIde());
+        minhasLinguagensFavoritas5.addAll(minhasLinguagensFavoritas3);
+        for (LinguagemFavorita linguagem : minhasLinguagensFavoritas5) System.out.println(linguagem);
+
+    }
+}
+
+class LinguagemFavorita implements Comparable<LinguagemFavorita> {
+    public String nome;
+    public Integer anoDeCriacao;
+    public String ide;
+
+    public LinguagemFavorita(String nome, Integer anoDeCriacao, String ide) {
+        this.nome = nome;
+        this.anoDeCriacao = anoDeCriacao;
+        this.ide = ide;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "nome='" + nome + '\'' +
+                ", anoDeCriacao=" + anoDeCriacao +
+                ", ide='" + ide + '\'' +
+                '}';
+    }
+
+    @Override
+    public int compareTo(LinguagemFavorita linguagemFavorita) {
+        return this.nome.compareTo(linguagemFavorita.nome);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LinguagemFavorita that = (LinguagemFavorita) o;
+        return nome.equals(that.nome);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome);
+    }
+}
+
+class ComparatorIde implements Comparator<LinguagemFavorita>{
+
+    @Override
+    public int compare(LinguagemFavorita lf1, LinguagemFavorita lf2) {
+        return lf1.ide.compareToIgnoreCase(lf2.ide);
+    }
+}
+
+class ComparatorAnoDeCriacaoENome implements Comparator<LinguagemFavorita>{
+
+    @Override
+    public int compare(LinguagemFavorita lf1, LinguagemFavorita lf2) {
+        int anoDeCriacao = Integer.compare(lf1.anoDeCriacao, lf2.anoDeCriacao);
+        if (anoDeCriacao != 0) return anoDeCriacao;
+        return lf1.nome.compareToIgnoreCase(lf2.nome);
+    }
+}
+
+class ComparatorNomeAnoDeCriacaoIde implements Comparator<LinguagemFavorita>{
+
+    @Override
+    public int compare(LinguagemFavorita lf1, LinguagemFavorita lf2) {
+        int nome = lf1.nome.compareToIgnoreCase(lf2.nome);
+        int anoDeCriacao = Integer.compare(lf1.anoDeCriacao, lf2.anoDeCriacao);
+        if (nome != 0) return nome;
+        if (anoDeCriacao != 0) return anoDeCriacao;
+        return lf1.ide.compareToIgnoreCase(lf2.ide);
     }
 }
