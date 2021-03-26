b0f8bc1bf54d4d100666ffc154087e821c2cb731
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2021-03-26 13:23:51 -0300

    Exercício Set

--- src/br/com/dio/collection/set/ExemploSet.java
+++ src/br/com/dio/collection/set/ExemploSet.java
@@ -1,21 +1,24 @@
 package br.com.dio.collection.set;
 
 
+import java.util.*;
+
 public class ExemploSet {
     public static void main(String[] args) {
 // Dada uma lista com 7 notas de um aluno [7, 8.5, 9.3, 5, 7, 0, 3.6], faça:
 
 //      Set notas = new HashSet(); //antes do java 5
-//      Set<Double> notas = new HashSet<>(); //Generics(jdk 5) - Diamont Operator(jdk 7)
 //      HashSet<Double> notas = new HashSet<>();
-//      Set<Double> notas = new HashSet<>(Arrays.asList(7d, 8.5, 9.3, 5d, 7d, 0d, 3.6));
+//      Set<Double> notas = new HashSet<>(); //Generics(jdk 5) - Diamont Operator(jdk 7)
 /*      Set<Double> notas = Set.of(7d, 8.5, 9.3, 5d, 7d, 0d, 3.6);
         notas.add(1d);
         notas.remove(5d);
         System.out.println(notas);
 */
 
-//        System.out.println("Crie um conjunto e adicione as notas: ");
+        System.out.println("Crie um conjunto e adicione as notas: ");
+        Set<Double> notas = new HashSet<>(Arrays.asList(7d, 8.5, 9.3, 5d, 7d, 0d, 3.6));
+        System.out.println(notas.toString());
 
 //        System.out.println("Exiba a posição da nota 5.0: ");
 
@@ -23,30 +26,59 @@ public class ExemploSet {
 
 //        System.out.println("Substitua a nota 5.0 pela nota 6.0: ");
 
-//        System.out.println("Confira se a nota 5.0 está no conjunto: ");
+        System.out.println("Confira se a nota 5.0 está no conjunto: " + notas.contains(5d));
 
 //        System.out.println("Exiba a terceira nota adicionada: ");
 
-//        System.out.println("Exiba a menor nota: ");
+        System.out.println("Exiba a menor nota: " + Collections.min(notas));
 
-//        System.out.println("Exiba a maior nota: ");
+        System.out.println("Exiba a maior nota: " + Collections.max(notas));
 
-//        System.out.println("Exiba a soma dos valores: ");
+        Iterator<Double> iterator = notas.iterator();
+        Double soma = 0.0;
+        while(iterator.hasNext()) {
+            Double next = iterator.next();
+            soma += next;
+        }
+        System.out.println("Exiba a soma dos valores: " + soma);
 
-//        System.out.println("Exiba a média das notas: ");
+        System.out.println("Exiba a média das notas: " + (soma/ notas.size()));
 
-//        System.out.println("Remova a nota 0: ");
+        System.out.println("Remova a nota 0: ");
+        notas.remove(0d);
+        System.out.println(notas);
 
 //        System.out.println("Remova a nota da posição 0");
 
-//        System.out.println("Remova as notas menores que 7 e exiba a lista: ");
+        System.out.println("Remova as notas menores que 7 e exiba a lista: ");
+        Iterator<Double> iterator1 = notas.iterator();
+        while(iterator1.hasNext()){
+            Double next = iterator1.next();
+            if (next < 7) iterator1.remove();
+        }
+        System.out.println(notas);
 
-//        System.out.println("Exiba todas as notas na ordem em que foram informados: ");
+        System.out.println("Exiba todas as notas na ordem em que foram informados: ");
+        Set<Double> notas2 = new LinkedHashSet<>();
+        notas2.add(7d);
+        notas2.add(8.5);
+        notas2.add(9.3);
+        notas2.add(5d);
+        notas2.add(7d);
+        notas2.add(0d);
+        notas2.add(3.6);
+        System.out.println(notas2);
 
-//        System.out.println("Exiba todas as notas na ordem crescente: ");
+        System.out.println("Exiba todas as notas na ordem crescente: ");
+        Set<Double> notas3 = new TreeSet<>(notas2);
+        System.out.println(notas3);
 
-//        System.out.println("Apague todo o conjunto");
+        System.out.println("Apague todo o conjunto");
+        notas.clear();
 
-//        System.out.println("Confira se o conjunto está vazio: ");
+        System.out.println("Confira se o conjunto está vazio: " + notas.isEmpty());
+        System.out.println("Confira se o conjunto 2 está vazio: " + notas2.isEmpty());
+        System.out.println("Confira se o conjunto 3 está vazio: " + notas3.isEmpty());
+
     }
 }
