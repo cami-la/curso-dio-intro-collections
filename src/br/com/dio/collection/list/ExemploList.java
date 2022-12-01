f77ce9fe2be0d0a8d2b1700d6b5c90494d079f0e
Bruna Quinto Marsiaj <110314990+Bruna-Quinto-Marsiaj@users.noreply.github.com>
2022-12-01 20:05:36 -0300

    conhecendo os metodos list

--- src/br/com/dio/collection/list/ExemploList.java
+++ src/br/com/dio/collection/list/ExemploList.java
@@ -11,39 +11,39 @@ class ExemploList {
 //      ArrayList<Double> notas = new ArrayList<>();
 //      List<Double> notas = new ArrayList<>(Arrays.asList(7d, 8.5, 9.3, 5d, 7d, 0d, 3.6));
         /*List<Double> notas = Arrays.asList(7d, 8.5, 9.3, 5d, 7d, 0d, 3.6);
-        notas.add(10d);
+        notas.add(10d); // assim eu não posso add, posso usar praticamente todos os outros métodos
         System.out.println(notas);*/
         /*List<Double> notas = List.of(7d, 8.5, 9.3, 5d, 7d, 0d, 3.6);
-        notas.add(1d);
+        notas.add(1d); // assim eu não posso add e remover, posso outros métodos
         notas.remove(5d);
         System.out.println(notas);*/
 
         System.out.println("Crie uma lista e adicione as sete notas: ");
 
         List<Double> notas = new ArrayList<Double>(); //Generics(jdk 5) - Diamond Operator(jdk 7)
-        notas.add(7.0);
+        notas.add(7.0); //n posso colocar só o 7 pq é double, então ou ponho como está ou ponho 7d. Foi usado o add(Double)
         notas.add(8.5);
         notas.add(9.3);
         notas.add(5.0);
         notas.add(7.0);
         notas.add(0.0);
         notas.add(3.6);
-        System.out.println(notas.toString());
+        System.out.println(notas.toString());//poderia usar só notas. Salva na lista na mesma ordem que foi add
 
         System.out.println("Exiba a posição da nota 5.0: " + notas.indexOf(5d));
 
         System.out.println("Adicione na lista a nota 8.0 na posição 4: ");
-        notas.add(4, 8d);
+        notas.add(4, 8d); //add (int i, Double e) posição e elemento, i: 4, e:8d
         System.out.println(notas);
 
         System.out.println("Substitua a nota 5.0 pela nota 6.0: ");
-        notas.set(notas.indexOf(5d), 6.0);
+        notas.set(notas.indexOf(5d), 6.0); //primeiro acha a posição da nota e depois diz a nova nota
         System.out.println(notas);
 
-        System.out.println("Confira se a nota 5.0 está na lista: " + notas.contains(5d));
+        System.out.println("Confira se a nota 5.0 está na lista: " + notas.contains(5d)); //dentro do parênteses vai o objeto, a nota 5
 
-        /*System.out.println("Exiba todas as notas na ordem em que foram informados: ");
-        for (Double nota : notas) System.out.println(nota);*/
+        System.out.println("Exiba todas as notas na ordem em que foram informados: ");
+        for (Double nota : notas) System.out.println(nota);// vai imprimir um abaixo do outro
 
         System.out.println("Exiba a terceira nota adicionada: " + notas.get(2));
         System.out.println(notas.toString());
